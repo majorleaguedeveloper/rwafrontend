db6d699283a2c6079bcf286baa1e560098821a99
majorleaguedeveloper <majorleaguedevelopers@gmail.com>
2025-05-30 16:45:59 +0300

    first commit

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native'; // <-- This line is missing
+
+const Index = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Index</Text>
+    </View>
+  );
+};
+
+export default Index;
